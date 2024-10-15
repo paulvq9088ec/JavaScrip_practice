b01d6af9ca7ad0abe287fabecf17cc5276f52796
paulvq9088ec <paulvq9088@hotmail.com>
2024-10-15 16:12:53 -0500

    Actualizacion

--- ejercicios/ejercicio8.js
+++ ejercicios/ejercicio8.js
@@ -2,44 +2,71 @@
 Debes hacer una función para filtrar a todas las personas que tengan el nombre "Luis" 
 y que esa misma función también muestre cuáles son mayores de edad
 */
+// function ejercicio8() {
+//   const personas = [
+//     {
+//       nombre: "Luis",
+//       apellido: "Fernandez",
+//       edad: 19,
+//     },
+//     {
+//       nombre: "Lucas",
+//       apellido: "Casilla",
+//       edad: 10,
+//     },
+//     {
+//       nombre: "Luis",
+//       apellido: "Lopez",
+//       edad: 29,
+//     },
+//     {
+//       nombre: "Carlo",
+//       apellido: "Veintimilla",
+//       edad: 45,
+//     },
+//     {
+//       nombre: "Maria",
+//       apellido: "Escobar",
+//       edad: 26,
+//     },
+//   ];
+//   parrafo.innerHTML += 'Estas son las persona que llevan el nombre "Luis":'+ '<br>'+ '<lu>';
+//   personas.filter((e) => {
+//     if (e.nombre === "Luis") parrafo.innerHTML += '<li>' + (`${e.nombre} ${e.apellido}`+'</li></lu>'+ '<br>')
+//   });
+//   parrafo.innerHTML += 'Las personas mayores de edad son las siguientes:'+ '<br>'+ '<lu>';
+//   personas.filter((e)=> {
+//     if (e.edad >= 18) parrafo.innerHTML += '<li>' + (`${e.nombre} ${e.apellido}`+'</li></lu>'+ '<br>')
+//   });
+// }
+
+// //Declarar
+// let parrafo = document.getElementById('result');
+// ejercicio8()
+
 function ejercicio8() {
-  const personas = [
+  const notasAlumno = [
     {
-      nombre: "Luis",
-      apellido: "Fernandez",
-      edad: 19,
+      nombre: "Alex",
+      curso: 1,
+      nota: 9,
     },
     {
-      nombre: "Lucas",
-      apellido: "Casilla",
-      edad: 10,
+      nombre: "Pepito",
+      curso: 3,
+      nota: 5,
     },
     {
-      nombre: "Luis",
-      apellido: "Lopez",
-      edad: 29,
-    },
-    {
-      nombre: "Carlo",
-      apellido: "Veintimilla",
-      edad: 45,
-    },
-    {
-      nombre: "Maria",
-      apellido: "Escobar",
-      edad: 26,
+      nombre: "Juanito",
+      curso: 2,
+      nota: 6,
     },
   ];
-  parrafo.innerHTML += 'Estas son las persona que llevan el nombre "Luis":'+ '<br>'+ '<lu>';
-  personas.filter((e) => {
-    if (e.nombre === "Luis") parrafo.innerHTML += '<li>' + (`${e.nombre} ${e.apellido}`+'</li></lu>'+ '<br>')
-  });
-  parrafo.innerHTML += 'Las personas mayores de edad son las siguientes:'+ '<br>'+ '<lu>';
-  personas.filter((e)=> {
-    if (e.edad >= 18) parrafo.innerHTML += '<li>' + (`${e.nombre} ${e.apellido}`+'</li></lu>'+ '<br>')
+  notasAlumno.filter((e) => {
+    if (e.nota < 8) {
+      console.log(`El estudiante ${e.nombre}, del curso ${e.curso} está peligrando el curso! Hay que estudiar`);
+    }
   });
 }
 
-//Declarar
-let parrafo = document.getElementById('result');
-ejercicio8()
+ejercicio8();
---
+++ ejercicios/jugarBotone.js
@@ -0,0 +1,22 @@
+let alumnosBaile = [
+  {
+    name: Andrea,
+    curso: "Salsa",
+    horario: "lun, mier",
+    hora: 7,
+  },
+  {
+    name: Juan,
+    curso: "Bachata",
+    horario: "mar, jue",
+    hora: 8,
+  },
+  {
+    name: Sebastian,
+    curso: "Salsa",
+    horario: "sab",
+    hora: 10,
+  },
+];
+
+

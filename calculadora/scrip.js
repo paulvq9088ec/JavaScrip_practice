2821948440b4175f5291d0e03f57dfea6ae4e371
paulvq9088ec <paulvq9088@hotmail.com>
2024-10-18 11:35:42 -0500

    segundo día y culminación turorial

--- calculadora/scrip.js
+++ calculadora/scrip.js
@@ -2,7 +2,7 @@ const botonNumero = document.querySelectorAll('[data-numero]');
 const botonOperador = document.querySelectorAll('[data-operador]');
 const botonIgual = document.querySelector('[data-igual]');
 const botonBorrarTodo = document.querySelector('[data-borrar-todo]');
-const botonBorrar = document.querySelectorAll('[data-borrar]');
+const botonBorrar = document.querySelector('[data-borrar]');
 const textoValorSuperior = document.querySelector('[data-valor-superior]');
 const textoValorInferior = document.querySelector('[data-valor-inferior]');
 
@@ -21,7 +21,55 @@ class Calculadora {
 
     imprimirDisplay(){
         this.textoValorInferior.innerText = this.valorInferior
+        this.textoValorSuperior.innerText = this.valorSuperior
     };
+
+    borrar(){
+        this.valorInferior = this.valorInferior.slice(0,-1)
+    };
+
+    elergirOperador(operador){
+        if(this.valorInferior == '') return
+        if(this.valorSuperior != '') {
+            this.realizarCalculo()
+        }
+        this.operador = operador
+        this.valorSuperior = this.valorInferior
+        this.valorInferior = ''
+    };
+
+    realizarCalculo(){
+        let resultado
+        let conversionValorSuperior = parseFloat(this.valorSuperior)
+        let conversionValorInferior = parseFloat(this.valorInferior)
+
+        switch (this.operador){
+            case '+':
+                resultado = conversionValorSuperior + conversionValorInferior
+            break
+            case '-':
+                resultado = conversionValorSuperior + conversionValorInferior
+            break
+            case '+':
+                resultado = conversionValorSuperior * conversionValorInferior
+            break
+            case '/':
+                resultado = conversionValorSuperior / conversionValorInferior
+            break
+            default: return
+        }
+
+        this.valorInferior = resultado.toFixed(13)
+        this.operador = undefined
+        this.valorSuperior = ''
+    };
+
+    limpiarPantalla(){
+        this.valorInferior = ''
+        this.valorSuperior = ''
+        this.operador = undefined
+    }
+
 };
 
 const calculadora = new Calculadora (textoValorInferior,textoValorSuperior)
@@ -32,4 +80,27 @@ botonNumero.forEach(boton => {
         calculadora.imprimirDisplay();
         // console.log(boton.innerText)
     });
+});
+
+botonBorrar.addEventListener('click', () => {
+    calculadora.borrar();
+    calculadora.imprimirDisplay();
+});
+
+
+botonOperador.forEach(boton => {
+    boton.addEventListener('click', () => {
+        calculadora.elergirOperador(boton.innerText);
+        calculadora.imprimirDisplay();
+    });
+});
+
+botonIgual.addEventListener('click', () => {
+    calculadora.realizarCalculo();
+    calculadora.imprimirDisplay();
+});
+
+botonBorrarTodo.addEventListener('click', () => {
+    calculadora.limpiarPantalla();
+    calculadora.imprimirDisplay();
 });
